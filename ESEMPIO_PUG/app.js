75f11599df11fb6d45f043eccb193738db0b3a9e
CarpioNick4 <carpio.nick@iisgalvanimi.edu.it>
2024-02-27 11:07:45 +0000

    finito

--- ESEMPIO_PUG/app.js
+++ ESEMPIO_PUG/app.js
@@ -1,11 +1,35 @@
-var express = require('express');
-var app = express();
-app.set('view engine', 'pug'); //Dico a express di usare pug come motore di template
-app.use(express.static(__dirname + '/public')); // Dico ad express dove recuperare i file
-app.get('/', function (req, res) {
-//res.send('Ciao Mondo');
-res.render('index'); //Dico a express di processare e inviare la pagina index.pug
+const express = require('express');
+const madrid = require('./json/madrid.json'); //Copia il file people.json dentro la variabile people
+const barca = require('./json/barca.json');
+
+
+const app = express();
+app.set('view engine', 'pug');
+app.use(express.static(__dirname + '/public'));
+
+app.get('/', (req, res) => {
+
+    res.render('index', {
+    title: 'Homepage',
+    rMadrid:madrid['Real Madrid'],
+    barcelona:barca.Barcelona
+    });
 });
+
+app.get('/madrid', (req, res) => {
+    res.render('squadre', {
+    title: 'THE REAL MADRID',
+    squadra:madrid['Real Madrid']
+    });
+});
+
+app.get('/barca', (req, res) => {
+    res.render('squadre', {
+    title: 'THE BARCELONA',
+    squadra:barca.Barcelona
+    });
+});
+
 app.listen(3000, function () {
 console.log('Example app listening on port 3000!');
 });
